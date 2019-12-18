6fef0ace4d2933285c46d124dc5e975f329a1430
chenxi <bbxycx.18@163.com>
2019-12-19 00:27:12 +0800

    优化代码 小程序登录

--- app/Middleware/BaseAuthMiddleware.php
+++ app/Middleware/BaseAuthMiddleware.php
@@ -122,7 +122,7 @@ class BaseAuthMiddleware implements MiddlewareInterface
         $currentPath = Common::getCurrentPath($this->request);
         $appMutex = env('APP_' . strtoupper($currentPath) . '_MUTEX', true);
         if ($appMutex) {
-            $redisToken = Redis::getContainer()->get(strtolower($currentPath) . '_token_' . $uuid);
+            $redisToken = Redis::getContainer()->get(strtolower($currentPath) . ':token:' . $uuid);
             if ($redisToken && $redisToken !== self::$authToken) {
                 throw new BusinessException(ErrorCode::UNAUTHORIZED, '您已在其他设备登录');
             }
