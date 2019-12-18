16c9684ea4e0196fd3bb905c81dd3e7fdcc485eb
swimmingcloud <1076137882@qq.com>
2019-12-18 17:34:05 +0800

    标签详情

--- app/Service/TagService.php
+++ app/Service/TagService.php
@@ -16,6 +16,19 @@ class TagService extends BaseService
     public $table;
 
     /**
+     * @Inject()
+     * @var UserTagService
+     */
+    public $userTagService;
+
+    /**
+     * @Inject()
+     * @var TagPostRelationService
+     */
+    public $tagPostRelationService;
+
+
+    /**
      * 标签列表
      * @param RequestInterface $request
      * @return \Psr\Http\Message\ResponseInterface
@@ -34,13 +47,50 @@ class TagService extends BaseService
     }
 
     /**
+     * 标签详情
      * @param RequestInterface $request
      * @return mixed
      */
     public function show(RequestInterface $request)
     {
-        $this->condition = ['id' => $request->input('id')];
-        return parent::show($request);
+        try {
+            $uid = $request->getAttribute('uid', 0);
+            $id = $request->input('id');
+
+            $this->select = ['id', 'tag_name', 'is_hot', 'tag_type', 'used_count'];
+            $this->condition = [
+                ['id', '=', $id],
+                ['status', '=', 1],
+            ];
+
+            $data = parent::show($request);
+            $data['is_follow'] = 0;
+            if ($uid) {
+                // 查询是否关注
+                $this->userTagService->condition = [
+                    ['user_id', '=', $uid],
+                    ['tag_id', '=', $id]
+                ];
+                $exist = $this->userTagService->multiTableJoinQueryBuilder()->exists();
+                if ($exist) {
+                    $data['is_follow'] = 1;
+                }
+            }
+
+            //标签下帖子数
+            $this->tagPostRelationService->condition = ['tag_id' => $id];
+            $postNum = $this->userTagService->multiTableJoinQueryBuilder()->count();
+            $data['post_num'] = $postNum;
+
+            //标签关注人数
+            $this->userTagService->condition = ['tag_id' => $id];
+            $followedNum = $this->userTagService->multiTableJoinQueryBuilder()->count();
+            $data['followed_num'] = $followedNum;
+            return $data;
+
+        } catch (\Exception $e) {
+            throw new BusinessException((int)$e->getCode(), $e->getMessage());
+        }
     }
 
     /**
