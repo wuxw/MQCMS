cd25d441e7596d0255dd2b645af1ec2723e18d2b
chenxi <bbxycx.18@163.com>
2019-12-24 21:17:58 +0800

    tag model

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -4,15 +4,18 @@ declare (strict_types=1);
 namespace App\Model;
 
 /**
- * @property int $id 
- * @property int $first_create_user_id 
- * @property string $tag_name 
- * @property int $is_hot 
- * @property int $tag_type 
- * @property int $status 
- * @property int $used_count 
  * @property int $created_at 
+ * @property int $first_create_user_id 
+ * @property int $id 
+ * @property int $is_hot 
+ * @property int $status 
+ * @property string $tag_desc 
+ * @property string $tag_keyword 
+ * @property string $tag_name 
+ * @property string $tag_title 
+ * @property int $tag_type 
  * @property int $updated_at 
+ * @property int $used_count 
  */
 class Tag extends Model
 {
@@ -28,11 +31,11 @@ class Tag extends Model
      *
      * @var array
      */
-    protected $fillable = ['id', 'first_create_user_id', 'tag_name', 'is_hot', 'tag_type', 'status', 'used_count', 'created_at', 'updated_at'];
+    protected $fillable = ['created_at', 'first_create_user_id', 'id', 'is_hot', 'status', 'tag_desc', 'tag_keyword', 'tag_name', 'tag_title', 'tag_type', 'updated_at', 'used_count'];
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'first_create_user_id' => 'integer', 'is_hot' => 'integer', 'tag_type' => 'integer', 'status' => 'integer', 'used_count' => 'integer', 'created_at' => 'integer', 'updated_at' => 'integer'];
+    protected $casts = ['created_at' => 'integer', 'first_create_user_id' => 'integer', 'id' => 'integer', 'is_hot' => 'integer', 'status' => 'integer', 'tag_type' => 'integer', 'updated_at' => 'integer', 'used_count' => 'integer'];
 }
--- migrations/2019_12_06_140548_create_tag_table.php
+++ migrations/2019_12_06_140548_create_tag_table.php
@@ -18,6 +18,9 @@ class CreateTagTable extends Migration
                 $table->bigIncrements('id');
                 $table->unsignedInteger('first_create_user_id')->nullable(false)->comment('第一个创建的人的用户id');
                 $table->string('tag_name', 64)->nullable(false)->comment('标签名称');
+                $table->string('tag_title', 64)->nullable(false)->comment('标签标题（seo）');
+                $table->string('tag_desc', 128)->nullable(false)->comment('标签描述（seo）');
+                $table->string('tag_keyword', 128)->nullable(false)->comment('标签关键词（seo）');
                 $table->unsignedTinyInteger('is_hot')->nullable(false)->default(0)->comment('是否热门 0：正常 1：热门');
                 $table->unsignedTinyInteger('tag_type')->nullable(false)->default(1)->comment('类型 1：系统创建2：用户创建');
                 $table->unsignedTinyInteger('status')->nullable(false)->default(1)->comment('状态 1：正常 0：禁用（删除）');
